6dfba1a111c0c494de7ac78623e182d445c04a4d
= <=>
2020-09-08 11:02:51 +0700

    upload tracker

---
+++ Tracker/run_cmd.py
@@ -0,0 +1,152 @@
+python tools/generate_detections.py     --model=networks/mars-small128.pb     --mot_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/data/cam_01 --output_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/deep_sort/output_generate/
+
+python tools/generate_detections.py     --model=networks/mars-small128.pb     --mot_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/data/cam_02 --output_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/deep_sort/output_generate/
+
+python tools/generate_detections.py     --model=networks/mars-small128.pb     --mot_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/data/cam_03 --output_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/deep_sort/output_generate/
+
+python tools/generate_detections.py     --model=networks/mars-small128.pb     --mot_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/data/cam_04 --output_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/deep_sort/output_generate/
+
+python tools/generate_detections.py     --model=networks/mars-small128.pb     --mot_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/data/cam_05 --output_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/deep_sort/output_generate/
+
+python tools/generate_detections.py     --model=networks/mars-small128.pb     --mot_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/data/cam_06 --output_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/deep_sort/output_generate/
+
+python tools/generate_detections.py     --model=networks/mars-small128.pb     --mot_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/data/cam_07 --output_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/deep_sort/output_generate/
+
+python tools/generate_detections.py     --model=networks/mars-small128.pb     --mot_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/data/cam_08 --output_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/deep_sort/output_generate/
+
+python tools/generate_detections.py     --model=networks/mars-small128.pb     --mot_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/data/cam_09 --output_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/deep_sort/output_generate/
+
+python tools/generate_detections.py     --model=networks/mars-small128.pb     --mot_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/data/cam_10 --output_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/deep_sort/output_generate/
+
+python tools/generate_detections.py     --model=networks/mars-small128.pb     --mot_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/data/cam_11 --output_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/deep_sort/output_generate/
+
+python tools/generate_detections.py     --model=networks/mars-small128.pb     --mot_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/data/cam_12 --output_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/deep_sort/output_generate/
+
+python tools/generate_detections.py     --model=networks/mars-small128.pb     --mot_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/data/cam_13 --output_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/deep_sort/output_generate/
+
+python tools/generate_detections.py     --model=networks/mars-small128.pb     --mot_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/data/cam_14 --output_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/deep_sort/output_generate/
+
+python tools/generate_detections.py     --model=networks/mars-small128.pb     --mot_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/data/cam_15 --output_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/deep_sort/output_generate/
+
+python tools/generate_detections.py     --model=networks/mars-small128.pb     --mot_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/data/cam_16 --output_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/deep_sort/output_generate/
+
+python tools/generate_detections.py     --model=networks/mars-small128.pb     --mot_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/data/cam_17 --output_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/deep_sort/output_generate/
+
+python tools/generate_detections.py     --model=networks/mars-small128.pb     --mot_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/data/cam_18 --output_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/deep_sort/output_generate/
+
+python tools/generate_detections.py     --model=networks/mars-small128.pb     --mot_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/data/cam_19 --output_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/deep_sort/output_generate/
+
+python tools/generate_detections.py     --model=networks/mars-small128.pb     --mot_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/data/cam_20 --output_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/deep_sort/output_generate/
+
+python tools/generate_detections.py     --model=networks/mars-small128.pb     --mot_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/data/cam_21 --output_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/deep_sort/output_generate/
+
+python tools/generate_detections.py     --model=networks/mars-small128.pb     --mot_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/data/cam_22 --output_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/deep_sort/output_generate/
+
+python tools/generate_detections.py     --model=networks/mars-small128.pb     --mot_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/data/cam_23 --output_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/deep_sort/output_generate/
+
+python tools/generate_detections.py     --model=networks/mars-small128.pb     --mot_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/data/cam_24 --output_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/deep_sort/output_generate/
+
+python tools/generate_detections.py     --model=networks/mars-small128.pb     --mot_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/data/cam_25 --output_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/deep_sort/output_generate/
+----------------------------------------------------------------
+python tools/generate_detections.py     --model=networks/mars-small128.pb     --mot_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/data/cam_08 --output_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/deep_sort/output_generate/
+
+python tools/generate_detections.py     --model=networks/mars-small128.pb     --mot_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/data/cam_20 --output_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/deep_sort/output_generate/
+
+python tools/generate_detections.py     --model=networks/mars-small128.pb     --mot_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/data/cam_22 --output_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/deep_sort/output_generate/
+
+python tools/generate_detections.py     --model=networks/mars-small128.pb     --mot_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/data/cam_25 --output_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/deep_sort/output_generate/
+---------------------------------
+
+python tools/generate_detections.py     --model=networks/mars-small128.pb     --mot_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/data/cam_03 --output_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/deep_sort/output_generate/
+
+python tools/generate_detections.py     --model=networks/mars-small128.pb     --mot_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/data/cam_13 --output_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/deep_sort/output_generate/
+
+python tools/generate_detections.py     --model=networks/mars-small128.pb     --mot_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/data/cam_19 --output_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/deep_sort/output_generate/
+
+
+
+----------------------------------- 4/9/2020------------------------
+
+python tools/generate_detections.py     --model=networks/mars-small128.pb     --mot_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/data/cam_01 --output_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/deep_sort/output_generate/
+
+python tools/generate_detections.py     --model=networks/mars-small128.pb     --mot_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/data/cam_02 --output_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/deep_sort/output_generate/
+
+python tools/generate_detections.py     --model=networks/mars-small128.pb     --mot_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/data/cam_04 --output_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/deep_sort/output_generate/
+
+python tools/generate_detections.py     --model=networks/mars-small128.pb     --mot_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/data/cam_06 --output_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/deep_sort/output_generate/
+
+---------------------------------
+
+python tools/generate_detections.py     --model=networks/mars-small128.pb     --mot_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/data/cam_07 --output_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/deep_sort/output_generate/
+
+python tools/generate_detections.py     --model=networks/mars-small128.pb     --mot_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/data/cam_09 --output_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/deep_sort/output_generate/
+
+python tools/generate_detections.py     --model=networks/mars-small128.pb     --mot_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/data/cam_10 --output_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/deep_sort/output_generate/
+
+python tools/generate_detections.py     --model=networks/mars-small128.pb     --mot_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/data/cam_12 --output_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/deep_sort/output_generate/
+
+
+--------------------------------------------------
+
+
+python tools/generate_detections.py     --model=networks/mars-small128.pb     --mot_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/data/cam_03 --output_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/deep_sort/output_generate/
+
+
+python tools/generate_detections.py     --model=networks/mars-small128.pb     --mot_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/data/cam_05 --output_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/deep_sort/output_generate/
+
+
+python tools/generate_detections.py     --model=networks/mars-small128.pb     --mot_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/data/cam_14 --output_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/deep_sort/output_generate/
+
+
+python tools/generate_detections.py     --model=networks/mars-small128.pb     --mot_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/data/cam_16 --output_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/deep_sort/output_generate/
+
+
+python tools/generate_detections.py     --model=networks/mars-small128.pb     --mot_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/data/cam_18 --output_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/deep_sort/output_generate/
+
+
+python tools/generate_detections.py     --model=networks/mars-small128.pb     --mot_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/data/cam_21 --output_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/deep_sort/output_generate/
+
+
+python tools/generate_detections.py     --model=networks/mars-small128.pb     --mot_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/data/cam_23 --output_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/deep_sort/output_generate/
+
+
+
+python tools/generate_detections.py     --model=networks/mars-small128.pb     --mot_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/data/cam_24 --output_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/deep_sort/output_generate/
+
+3,5,14,16,18,21,23,24
+-----------------------------------------------
+
+python tools/generate_detections.py     --model=networks/mars-small128.pb     --mot_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/data/cam_08 --output_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/deep_sort/output_generate/
+
+python tools/generate_detections.py     --model=networks/mars-small128.pb     --mot_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/data/cam_11 --output_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/deep_sort/output_generate/
+
+python tools/generate_detections.py     --model=networks/mars-small128.pb     --mot_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/data/cam_13 --output_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/deep_sort/output_generate/
+
+python tools/generate_detections.py     --model=networks/mars-small128.pb     --mot_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/data/cam_15 --output_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/deep_sort/output_generate/
+
+
+
+
+-------------------------------
+
+python tools/generate_detections.py     --model=networks/mars-small128.pb     --mot_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/data/cam_17 --output_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/deep_sort/output_generate/
+
+python tools/generate_detections.py     --model=networks/mars-small128.pb     --mot_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/data/cam_19 --output_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/deep_sort/output_generate/
+
+python tools/generate_detections.py     --model=networks/mars-small128.pb     --mot_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/data/cam_20 --output_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/deep_sort/output_generate/
+
+python tools/generate_detections.py     --model=networks/mars-small128.pb     --mot_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/data/cam_25 --output_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/deep_sort/output_generate/
+
+
+python tools/generate_detections.py     --model=networks/mars-small128.pb     --mot_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/data/cam_22 --output_dir=/home/mmlab/working/Challenges/AI_HCM_2020/Counting_Vehicles/Task03/deep_sort/output_generate/
+
+
+
+
+
+
+
+
+
+
+
